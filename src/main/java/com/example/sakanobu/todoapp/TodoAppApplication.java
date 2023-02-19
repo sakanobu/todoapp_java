0c4902559e8326b79138d7d4aa724a4842959025
sakanobu <grapejuice100percent@gmail.com>
2023-02-19 22:30:32 +0900

    インデントをタブではなくスペースに変更

--- src/main/java/com/example/sakanobu/todoapp/TodoAppApplication.java
+++ src/main/java/com/example/sakanobu/todoapp/TodoAppApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TodoAppApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(TodoAppApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(TodoAppApplication.class, args);
+  }
 }
