6b4aac40a36a48bd05185155aaa5748b80587535
sakanobu <grapejuice100percent@gmail.com>
2023-03-05 22:47:18 +0900

    filterList, sortList, statusList, priorityListのmodel.addAttribute()の重複をなくす実装に変更

--- src/main/java/com/example/sakanobu/todoapp/TasksController.java
+++ src/main/java/com/example/sakanobu/todoapp/TasksController.java
@@ -11,6 +11,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -21,10 +22,34 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("/todos")
 public class TasksController {
   private final TasksDao tasksDao;
+  private final List<String> filterList = List.of("未完了", "未削除", "全て");
+  private final List<String> sortList = List.of("期限日", "優先度", "作成日");
+  private final List<String> statusList = List.of("未完了", "完了", "削除済み");
+  private final List<String> priorityList = List.of("低", "中", "高");
 
   @Autowired
   public TasksController(TasksDao tasksDao) {
     this.tasksDao = tasksDao;
+  }
+
+  @ModelAttribute("filterList")
+  public List<String> getFilterList() {
+    return filterList;
+  }
+
+  @ModelAttribute("sortList")
+  public List<String> getSortList() {
+    return sortList;
+  }
+
+  @ModelAttribute("statusList")
+  public List<String> getStatusList() {
+    return statusList;
+  }
+
+  @ModelAttribute("priorityList")
+  public List<String> getPriorityList() {
+    return priorityList;
   }
 
   //  @GetMapping
@@ -42,13 +67,10 @@ public class TasksController {
     Task inputTask =
         new Task(null, "", "", "", LocalDate.now(), LocalDateTime.now(), LocalDateTime.now());
     List<Task> targetTasks = tasksDao.findByStatusUnfinishedOrderByDueDateAsc();
-    List<String> filterList = List.of("未完了", "未削除", "全て");
-    List<String> sortList = List.of("期限日", "優先度", "作成日");
 
     model.addAttribute("task", inputTask);
     model.addAttribute("targetTasks", targetTasks);
-    model.addAttribute("filterList", filterList);
-    model.addAttribute("sortList", sortList);
+
     return "todos";
   }
 
@@ -57,31 +79,27 @@ public class TasksController {
                            @RequestParam("title") String title, Model model) {
     if (bindingResult.hasErrors()) {
       List<Task> targetTasks = tasksDao.findByStatusUnfinishedOrderByDueDateAsc();
-      List<String> filterList = List.of("未完了", "未削除", "全て");
-      List<String> sortList = List.of("期限日", "優先度", "作成日");
 
       model.addAttribute("targetTasks", targetTasks);
       model.addAttribute("task", inputTask);
-      model.addAttribute("filterList", filterList);
-      model.addAttribute("sortList", sortList);
 
       return "todos";
     }
 
     Task task = new Task(null, title, "未完了", "中", LocalDate.now(), LocalDateTime.now(),
         LocalDateTime.now());
+
     tasksDao.create(task);
+
     return "redirect:/todos";
   }
 
   @GetMapping("/{id}/edit")
   public String editTaskForm(@PathVariable("id") Integer id, Model model) {
     Task task = tasksDao.findById(id);
-    List<String> statusList = List.of("未完了", "完了", "削除済み");
-    List<String> priorityList = List.of("低", "中", "高");
+
     model.addAttribute("task", task);
-    model.addAttribute("statusList", statusList);
-    model.addAttribute("priorityList", priorityList);
+
     return "todo_edit";
   }
 
@@ -94,22 +112,21 @@ public class TasksController {
                            LocalDate dueDate,
                            @RequestParam("createdAt") LocalDateTime createdAt, Model model) {
     if (bindingResult.hasErrors()) {
-      List<String> statusList = List.of("未完了", "完了", "削除済み");
-      List<String> priorityList = List.of("低", "中", "高");
       model.addAttribute("task", requestTask);
-      model.addAttribute("statusList", statusList);
-      model.addAttribute("priorityList", priorityList);
+
       return "todo_edit";
     }
-
     Task task = new Task(id, title, status, priority, dueDate, createdAt, LocalDateTime.now());
+
     tasksDao.update(task);
+
     return "redirect:/todos";
   }
 
   @DeleteMapping("/{id}")
   public String deleteTask(@PathVariable("id") Integer id) {
     tasksDao.delete(id);
+
     return "redirect:/todos";
   }
 }
